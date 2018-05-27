7f0c2eb5b53ef1a2a318940b2b6949131ab6dcc6
mavl <mvaronamac@gmail.coml>
2018-05-27 20:34:38 +0200

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+// Ngrx
+import { Store } from '@ngrx/store';
+
+// Redux
+import { AppState } from './../redux/app.state';
+import { Post } from './../redux/post/post.model';
+import { SelectSubredditAction } from './../redux/selected/selected.actions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  topics: Array<string> = ['angular2', 'reactjs', 'vuejs'];
+  selectTopic: FormControl;
+
+  posts: Array<Post> = new Array<Post>();
+
+  constructor (
+    private _store: Store<AppState>
+  ) {
+
+    this.selectTopic = new FormControl('', [Validators.required]);
+
+    this.selectTopic.valueChanges
+    .subscribe((value) => {
+      const action = new SelectSubredditAction({
+        name: value
+      });
+      this._store.dispatch(action);
+    });
+
+    this._store.select('posts')
+        .subscribe((posts) => {
+          this.posts = posts;
+          console.log(posts);
+        });
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+// Components
 import { AppComponent } from './app.component';
+
+// Modules
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material/material/material.module';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// Ngrx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+
+// Redux
+import { AppReducer } from './../redux/app.reducer';
+import { SelectedEffects } from './../redux/selected/selected.effects';
+
+// Services
+import { PostsService } from './services/posts.service';
+import { PostEffects } from '../redux/post/post.effects';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot( AppReducer ),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25 //  Retains last 25 states
+    }),
+    EffectsModule.forRoot([
+      PostEffects,
+      SelectedEffects
+    ])
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
